b3c721be8507801e888f7e57f23e69739339cd7b
Olasubomi Awodipe <subomiawodipe1@gmail.com>
2024-06-25 11:02:58 -0700

    Added a script for basic player movement and a player placeholder

---
+++ Journey Through Amani/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+ 
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.velocity.normalized * speed * Time.deltaTime + rb.position);
+    }
+}
