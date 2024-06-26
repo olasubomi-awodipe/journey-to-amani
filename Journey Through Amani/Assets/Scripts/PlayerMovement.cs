efaca39d2758a9e225856a3d58ff35f1f5a19605
Olasubomi Awodipe <subomiawodipe1@gmail.com>
2024-06-26 01:26:30 -0700

    Implemented the flipping of the sprite based on the direction

--- Journey Through Amani/Assets/Scripts/PlayerMovement.cs
+++ Journey Through Amani/Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,39 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float speed = 5f;
+    public float speed = 1f;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-
-    // Update is called once per frame
     private void Update()
     {
- 
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (Camera.main.orthographicSize != 5.0f)
+        {
+            Debug.Log("Camera size changed to: " + Camera.main.orthographicSize);
+        }
+
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = movement;
+
+        // Flip the sprite based on movement direction
+        if (movement.x > 0)
+        {
+            gameObject.transform.localScale = new Vector2(-1, 1);
+        }
+        else if (movement.x < 0)
+        {
+            gameObject.transform.localScale = new Vector2(1, 1);
+
+        }
     }
+
+
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.velocity.normalized * speed * Time.deltaTime + rb.position);
+        rb.MovePosition(rb.velocity.normalized * speed * Time.fixedDeltaTime + rb.position);
     }
 }
