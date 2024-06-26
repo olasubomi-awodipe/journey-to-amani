55d4ae1f7e0a3a7a0c6cf216facfa3a0f406cb3c
Olasubomi Awodipe <subomiawodipe1@gmail.com>
2024-06-26 01:48:31 -0700

    Updated the player flipping

--- Journey Through Amani/Assets/Scripts/PlayerMovement.cs
+++ Journey Through Amani/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed = 1f;
+    private bool facingLeft = true;
 
     private void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
+
 
     private void Update()
     {
@@ -23,21 +25,29 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = movement;
 
         // Flip the sprite based on movement direction
-        if (movement.x > 0)
+        if (movement.x > 0 && facingLeft)
         {
-            gameObject.transform.localScale = new Vector2(-1, 1);
+            Flip();
         }
-        else if (movement.x < 0)
+        else if (movement.x < 0 && !facingLeft) 
         {
-            gameObject.transform.localScale = new Vector2(1, 1);
-
+            Flip();
         }
     }
-
 
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.velocity.normalized * speed * Time.fixedDeltaTime + rb.position);
+    }
+
+
+    private void Flip()
+    {
+        Vector2 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingLeft = !facingLeft;
     }
 }
