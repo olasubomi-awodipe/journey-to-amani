a259a1ff44e9f7ccb3474fda60c4fafdac8e305a
Melaniej333 <melaniejackson@spelman.edu>
2024-07-01 11:53:12 -0700

    fixed the npc script
    new script for npc

--- Journey Through Amani/Assets/Scripts/NPC1.cs
+++ Journey Through Amani/Assets/Scripts/NPC1.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,66 +6,273 @@ using UnityEngine.UI;
 public class NPC1 : MonoBehaviour
 {
     public GameObject dialoguePanel;
-    public Text dialoguetext;
+    public GameObject contButton;
+    public Text dialoguetext; // Ensuring the variable name matches the UI element
     public string[] dialogue;
     private int index;
     public float wordSpeed;
     public bool playerIsClose;
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        // Optionally initialize any variables if needed
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E)&& playerIsClose){
-            if(dialoguePanel.activeInHierarchy){
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
                 zeroText();
-
-
             }
-            else{
+            else
+            {
                 dialoguePanel.SetActive(true);
-                StartCorouting (Typing());
+                StartCoroutine(Typing());
             }
         }
-        
+        if (dialoguetext.text == dialogue[index]){
+            contButton.SetActive(true);
+        }
     }
 
-    public void zeroText(){
-        dialoguetext.text="";
+    public void zeroText()
+    {
+        dialoguetext.text = "";
         index = 0;
         dialoguePanel.SetActive(false);
-
     }
 
-    IEnumerator Typing(){
-        foreach(char letter in dialogue[index].ToCharArray()){
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
             dialoguetext.text += letter;
             yield return new WaitForSeconds(wordSpeed);
         }
     }
 
-    public void NextLine(){
-        if(index < dialogue.length-1){
+    public void NextLine()
+    {
+
+        contButton.SetActive(false);
+        if (index < dialogue.Length - 1)
+        {
             index++;
-            dialoguetext.text ="";
-            StartCorouting (Typing());
+            dialoguetext.text = "";
+            StartCoroutine(Typing());
         }
-        else{
+        else
+        {
             zeroText();
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other){
-        if(other.CompareTag("Playetr")){
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
             playerIsClose = true;
         }
     }
-    private void OnTriggerExitr2D(Collider2D other){
-        if(other.CompareTag("Playetr")){
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
+    }
+}
+*/
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // Import the TextMesh Pro namespace
+
+public class NPC1 : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public GameObject contButton;
+    public TMP_Text dialoguetext; // Change Text to TMP_Text
+    public string[] dialogue;
+    private int index;
+    public float wordSpeed;
+    public bool playerIsClose;
+
+    void Start()
+    {
+        dialoguePanel.SetActive(false);
+        contButton.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+        
+        if (dialoguetext.text == dialogue[index])
+        {
+            contButton.SetActive(true);
+        }
+    }
+
+    public void zeroText()
+    {
+        dialoguetext.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+        contButton.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        contButton.SetActive(false);
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialoguetext.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        contButton.SetActive(false);
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialoguetext.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered trigger");
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exited trigger");
+            playerIsClose = false;
+            zeroText();
+        }
+    }
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // Import the TextMesh Pro namespace
+
+public class NPC1 : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public GameObject contButton;
+    public TMP_Text dialoguetext; // Change Text to TMP_Text
+    public string[] dialogue;
+    private int index;
+    public float wordSpeed;
+    public bool playerIsClose;
+
+    void Start()
+    {
+        dialoguePanel.SetActive(false);
+        contButton.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+        
+        if (dialoguetext.text == dialogue[index])
+        {
+            contButton.SetActive(true);
+        }
+    }
+
+    public void zeroText()
+    {
+        dialoguetext.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+        contButton.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        contButton.SetActive(false);
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialoguetext.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        contButton.SetActive(false);
+        if (index < dialogue.Length - 1) // Corrected from dialogue.length to dialogue.Length
+        {
+            index++;
+            dialoguetext.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered trigger");
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exited trigger");
             playerIsClose = false;
             zeroText();
         }
